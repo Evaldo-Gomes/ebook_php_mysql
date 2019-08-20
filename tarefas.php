ab6341a1641b8d86b202fbe9cfdbb2a20cf2165a
Evaldo <evaldo_gomes@hotmail.com>
2019-08-20 15:50:09 -0300

    projeto lista de tarefas

---
+++ tarefas.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>Gerenciador de Tarefas</title>
+    </head>
+    <body>
+        <h1>Gerenciador de tarefas</h1>
+        <form>
+            <fieldset>
+                <legend>Nova tarefa</legend>
+                <label>
+                    Tarefa
+                    <input type="text" name="nome" />
+                </label>
+                <input type="submit" value="cadastrar" />
+            </fieldset>
+        </form>
+    </body>
+</html>
