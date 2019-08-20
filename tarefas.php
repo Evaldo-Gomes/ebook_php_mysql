e87844dfbb89bd1f087f3b7a07520e4ae3b08423
Evaldo <evaldo_gomes@hotmail.com>
2019-08-20 16:35:47 -0300

    entendendo o comando  do PHP

--- tarefas.php
+++ tarefas.php
@@ -1,9 +1,11 @@
+<?php session_start(); ?>
 <html>
     <head>
         <title>Gerenciador de Tarefas</title>
     </head>
     <body>
         <h1>Gerenciador de tarefas</h1>
+
         <form>
             <fieldset>
                 <legend>Nova tarefa</legend>
@@ -14,5 +16,30 @@
                 <input type="submit" value="cadastrar" />
             </fieldset>
         </form>
+
+        <?php
+
+        if (isset($_GET['nome'])){
+            $_SESSION['lista_tarefas'][] = $_GET['nome'];
+        }
+
+        if (isset($_SESSION['lista_tarefas'])){
+            $lista_tarefas = $_SESSION['lista_tarefas'];
+        }
+
+        ?>
+
+        <table>
+            <tr>
+                <th>Tarefas</th>
+            </tr>
+
+            <?php foreach ($lista_tarefas as $tarefa) : ?>
+                <tr>
+                    <td><?php echo $tarefa; ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+        
     </body>
 </html>
